27b83b34f5479b105337b963b16de66a96ebcffe
DDesta25 <145198978+DDesta25@users.noreply.github.com>
2024-01-29 19:30:41 -0500

    filled out the schema.sql and added the three tables needed

---
+++ db/schema.sql
@@ -0,0 +1,33 @@
+DROP DATABASE IF EXISTS Lions_Lost;
+CREATE DATABASE Lions_Lost;
+USE Lions_Lost;
+
+
+CREATE TABLE department(
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE role (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL,
+    department_id INT,
+    foreign key (department_id)
+    references department (id)
+    on delete set NULL 
+
+);
+CREATE TABLE employee(
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30),
+    role_id INT,
+    foreign key (role_id) 
+    references role (id) 
+on delete set null,
+manager_id INT,
+foreign key (manager_id)
+references employee (id)
+on delete set null
+);
