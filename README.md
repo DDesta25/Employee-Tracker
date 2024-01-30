2b2c2feeeb4fca5a703f3dcbc0fc86dad953d7d3
DDesta25 <145198978+DDesta25@users.noreply.github.com>
2024-01-30 15:11:21 -0500

    added the seeds.sql and added the department values, roles and salary

--- README.md
+++ README.md
@@ -30,7 +30,7 @@ npm i
   
   ## License
   
-A short and simple permissive license with conditions only requiring preservation of copyright and license notices. Licensed works, modifications, and larger works may be distributed under different terms and without source code.
+
   [https://choosealicense.com/licenses/mit/](https://choosealicense.com/licenses/mit/)
 
   
---
+++ startup.js
@@ -0,0 +1,21 @@
+// // get inquirer
+
+// function getAllEmployees(params) {
+//     db.query(`select * from employees`,(err, results)=>{
+// console.log(results);
+//     })
+// }
+
+// //* prompt with 
+// // list :
+// //  view employees
+// // view departments
+// // view roles 
+
+// switch (keys) {
+//     case `view employees`:
+//         getAllEmployees();
+//         break;
+//         default:
+//             break;
+// } 
