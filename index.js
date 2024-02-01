6c838cb38d94fa4ad577cca466244c32ba0c777e
DDesta25 <145198978+DDesta25@users.noreply.github.com>
2024-02-01 15:32:48 -0500

    added a function for the roles and departments to add for each prompt

--- index.js
+++ index.js
@@ -1,87 +1,184 @@
 const inquirer = require("inquirer");
 const mysql = require("mysql2");
+require("dotenv").config();
+
+const db = mysql.createConnection(
+  {
+    host: "localhost",
+    // MySQL username,
+    user: "root",
+    // MySQL password
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+  },
+  console.log(`Connected to the Employee_Tracker database.`)
+);
 
 const prompt = () => {
-  inquirer.prompt([
-    {
-      type: "list",
-      name: "choices",
-      message: "what would you like to do",
-      choices: [
-        { name: "view all departments", value: "VIEW_DEPARTMENTS" },
-        { name: "view all employees", value: "VIEW_EMPLOYEES" }
-        { name: "view all roles", value: "VIEW_ROLES" },
-        { name: "view all employee_role", value: "VIEW_EMPLOYEE_ROLE" },
-        { name: "add a department", value: "ADD_DEPARTMENT" },
-        { name: "add a role", value: "ADD_ROLE"},
-        { name: "add an employee", value: "ADD_EMPLOYEE"},
-        { name: "update an employee role", value: "UPDATE_EMPLOYEE_ROLE" },
-      ],
-    },
-  ])
-  .then((res)=> {
-let choice = res.choice 
-switch (choice) {
-    case `VIEW_EMPLOYEES`:
-    //   getAllEmployees();
-    console.log(choice);
-      break;
-    case `view roles`:
-      getAllRoles();
-      break;
-  
-    default:
-      break;
-  }
-  } )
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        name: "choices",
+        message: "what would you like to do",
+        choices: [
+          { name: "view all departments", value: "VIEW_DEPARTMENTS" },
+          { name: "view all employees", value: "VIEW_EMPLOYEES" },
+          { name: "view all roles", value: "VIEW_ROLES" },
 
+          { name: "add a department", value: "ADD_DEPARTMENT" },
+          { name: "add a role", value: "ADD_ROLE" },
+          { name: "add an employee", value: "ADD_EMPLOYEE" },
+          { name: "update an employee role", value: "UPDATE_EMPLOYEE_ROLE" },
+        ],
+      },
+    ])
+    .then((res) => {
+      let choice = res.choices;
+      switch (choice) {
+        case `VIEW_EMPLOYEES`:
+          getAllEmployees();
+          break;
+        case `VIEW_ROLES`:
+          getAllRoles();
+          break;
+        case `ADD_ROLE`:
+          addRole();
+          break;
+        case `ADD_DEPARTMENT`:
+          addDepartment();
+          break;
+        case `ADD_EMPLOYEE`:
+          addEmployee();
+          break;
+        case `VIEW_DEPARTMENTS`:
+          viewDepartment();
+          break;
+        case `UPDATE_EMPLOYEE_ROLE`:
+          updateEmployeeRole();
+          break;
+        default:
+          break;
+      }
+    });
 };
 
-/* prompt with -- 
-  List:
-  - view employees
-  - view departments
-  - view roles
-  - add employee
-  - change role
-  - add role
-*/
-
 // This will have the initial prompt for "What do you want to do?"
-start();
+prompt();
 
 /** ToDo --eventually I have a Join that shows the department name */
-function getAllEmployees(params) {
-  db.query(`select * from employees`, (err, results) => {
+function getAllEmployees() {
+  db.query(`select * from employee`, (err, results) => {
     console.table(results);
-    start();
+    console.log("view all employee");
+    prompt();
   });
 }
 
-function getAllRoles(params) {
-  db.query(`select * from roles`, (err, results) => {
+function getAllRoles() {
+  db.query(`select * from role`, (err, results) => {
     console.table(results);
-    start();
+    console.log("view all role");
+    prompt();
   });
 }
-// function to query fro all employees and log the table
-switch (key) {
-  case `view employees`:
-    getAllEmployees();
-    break;
-  case `view roles`:
-    getAllRoles();
-    break;
 
-  default:
-    break;
+function viewDepartment() {
+  db.query(`select * from department`, (err, results) => {
+    console.table(results);
+    console.log("view all department");
+    prompt();
+  });
 }
 
-// Here are the bonus options for Employee Tracker:
-// update employee managers, view employees by manager,
-//  view employees by department, departments, roles, and employees,
-// view the total utilized budget of a department
+function addDepartment() {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "name",
+        message: "whats the name of the new department",
+      },
+    ])
+    .then((answers) => {
+      db.query("insert into department set ?", answers, (err, results) => {
+        if (err) throw err;
+        console.table(results);
+        prompt();
+      });
+    });
+}
 
-// view all departments,
-//  view all roles, view all employees, add a department, add a role, add an employee,
-// and update an employee role
+function addRole() {
+  db.query("select * from department", (err, results)=>
+  {
+    const departmentArray = results.map(department=>({
+      name: department.name,
+      value: department.id
+    }))
+ 
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "title",
+        message: "whats the title of the role",
+      
+      },
+      {
+      type: 'number',
+      name: 'salary',
+      message: "what is the salary"
+      },
+      {
+        type: 'list',
+      name: 'department_id',
+      message: "what department is this role in",
+      choices: departmentArray
+      
+      }
+    ])
+    .then((answers) => {
+      db.query("insert into role set ?", answers, (err, results) => {
+        if (err) throw err;
+        console.table(results);
+        prompt();
+      });
+    });
+  })
+}
+
+function addEmployee() {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "name",
+        message: "whats the name of the new department",
+      },
+    ])
+    .then((answers) => {
+      db.query("insert into department set ?", answers, (err, results) => {
+        if (err) throw err;
+        console.table(results);
+        prompt();
+      });
+    });
+}
+function updateEmployeeRole() {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "name",
+        message: "whats the name of the new department",
+      },
+    ])
+    .then((answers) => {
+      db.query("insert into department set ?", answers, (err, results) => {
+        if (err) throw err;
+        console.table(results);
+        prompt();
+      });
+    });
+}
